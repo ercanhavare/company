4af322a4f9f0bd9b75faecbb25d6d2038c8c48de
Ercan Havare <ercanhavare@gmail.com>
2020-06-07 13:45:05 +0300

    PHPUnit Testing OrderTest

--- abc/tests/Order/OrderTest.php
+++ abc/tests/Order/OrderTest.php
@@ -1,12 +1,31 @@
 <?php
 
+use App\Entity\Order;
 use PHPUnit\Framework\TestCase;
 
 class OrderTest extends TestCase
 {
-    public function testAdd()
+    protected $order;
+
+    public function setUp()
     {
-        $result = 5 + 9;
-        $this->assertEquals(14, $result);
+        $this->order = new Order();
+    }
+
+    public function testThatCanGetTheOrderCode()
+    {
+        $this->order->setOrderCode("PathTech2020");
+
+        $this->assertEquals($this->order->getOrderCode(), "PathTech2020");
+    }
+
+    public function testNegativeOrderCodeForClassHasAttribute()
+    {
+        $this->assertClassNotHasAttribute("order_code", Order::class, "Order class doesn't has order_code as attribute");
+    }
+
+    public function testPositiveOrderCodeForClassHasAttribute()
+    {
+        $this->assertClassHasAttribute("orderCode", Order::class, "Order class doesn't has orderCode as attribute");
     }
 }
